9aa9c2879b8ea2d80f00d58513248c82f8f6952f
Andrea Muttoni <maverickmuttoni+github@gmail.com>
2022-09-22 16:56:08 +0200

    Update useGetEthPrice.ts

--- site/hooks/useGetEthPrice.ts
+++ site/hooks/useGetEthPrice.ts
@@ -1,19 +1,19 @@
 import { useEffect, useState } from "react";
 
-export default function useGetEthPrice() {
+export default function useGetFlowPrice() {
   const [price, setPrice] = useState("...");
 
   useEffect(() => {
     async function getPrice() {
       const raw = await fetch(
-        "https://api.coinbase.com/v2/prices/eth-usd/spot"
+        "https://api.coinbase.com/v2/prices/flow-usd/spot"
       );
       const { data } = await raw.json();
       setPrice(data.amount);
     }
 
     getPrice(); // initial call
-    const interval = setInterval(() => getPrice(), 5000);
+    const interval = setInterval(() => getPrice(), 10000);
 
     return () => clearInterval(interval);
   }, []);
